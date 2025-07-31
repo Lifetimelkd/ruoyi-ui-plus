51cc7efd11adaeaf0eddd25c4614af632258a80a
Lifetimelkd <770276349@qq.com>
2025-07-31 17:49:20 +0800

    创建ruoyi-vue-plus文件

---
+++ ruoyi-app-plus/config.js
@@ -0,0 +1,34 @@
+// 应用全局配置
+module.exports = {
+  // baseUrl: 'https://vue.ruoyi.vip/prod-api',
+  baseUrl: 'http://localhost:8080',
+  //客户端id
+  clientId:'428a8310cd442757ae699df5d894f051',
+  //授权模式
+  grantType:'password',
+  //租户id
+  tenantId: '000000',
+  //记住我模式
+  rememberMe: false,
+  // 应用信息
+  appInfo: {
+    // 应用名称
+    name: "ruoyi-app-plus",
+    // 应用版本
+    version: "1.1.0",
+    // 应用logo
+    logo: "/static/logo.png",
+    // 官方网站
+    site_url: "http://ruoyi.vip",
+    // 政策协议
+    agreements: [{
+        title: "隐私政策",
+        url: "https://ruoyi.vip/protocol.html"
+      },
+      {
+        title: "用户服务协议",
+        url: "https://ruoyi.vip/protocol.html"
+      }
+    ]
+  }
+}
---
+++ ruoyi-app-plus/plugins/index.js
@@ -0,0 +1,18 @@
+import tab from './tab'
+import auth from './auth'
+import modal from './modal'
+
+/**
+ * Vue 混入 组件之间共享 JavaScript 方法和属性 使用方式this.$tab
+ * @param {Object} Vue
+ */
+export default {
+  install(Vue) {
+    // 页签操作
+    Vue.prototype.$tab = tab
+    // 认证对象
+    Vue.prototype.$auth = auth
+    // 模态框对象
+    Vue.prototype.$modal = modal
+  }
+}
